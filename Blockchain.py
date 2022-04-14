c71e4503ec55a43ae7b82747b13c086b427fe31a
Subxmi <subo@subxmi.com>
2022-04-14 13:01:49 -0400

    main

--- Blockchain.py
+++ Blockchain.py
@@ -4,7 +4,7 @@ from time import time
 
 
 # Block class
-class Block(object):
+class Blockchain(object):
     def __init__(self):
         # Block header
         self.chain = []
@@ -13,3 +13,58 @@ class Block(object):
         self.new_block (previous_hash= "Thr Times 01/01/2022 Subxmi brink of second Bailout for Banks.", 
         proof = 100)
 
+    def new_block(self, proof, previous_hash=None):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.pending_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+        self.pending_transactions = []
+        self.chain.append(block)
+
+        return block
+
+#Search the blockchain for the most recent block.
+
+    @property
+    def last_block(self):
+ 
+        return self.chain[-1]
+
+# Add a transaction with relevant info to the 'blockpool' - list of pending tx's. 
+
+    def new_transaction(self, sender, recipient, amount):
+        transaction = {
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount
+        }
+        self.pending_transactions.append(transaction)
+        return self.last_block['index'] + 1
+
+# receive one block. Turn it into a string, turn that into Unicode (for hashing). Hash with SHA256 encryption, then translate the Unicode into a hexidecimal string.
+
+    def hash(self, block):
+        string_object = json.dumps(block, sort_keys=True)
+        block_string = string_object.encode()
+
+        raw_hash = hashlib.sha256(block_string)
+        hex_hash = raw_hash.hexdigest()
+
+        return hex_hash
+
+
+blockchain = Blockchain()
+t1 = blockchain.new_transaction("Satoshi", "Mike", '5 BTC')
+t2 = blockchain.new_transaction("Mike", "Satoshi", '1 BTC')
+t3 = blockchain.new_transaction("Satoshi", "Hal Finney", '5 BTC')
+blockchain.new_block(12345)
+
+t4 = blockchain.new_transaction("Mike", "Alice", '1 BTC')
+t5 = blockchain.new_transaction("Alice", "Bob", '0.5 BTC')
+t6 = blockchain.new_transaction("Bob", "Mike", '0.5 BTC')
+blockchain.new_block(6789)
+
+print("Genesis block: ", blockchain.chain)
