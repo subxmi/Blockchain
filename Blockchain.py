a718929f4df55077c12cdf9597ef0efb7229fa1a
Subxmi <subo@subxmi.com>
2022-04-14 12:59:41 -0400

    main

---
+++ Blockchain.py
@@ -0,0 +1,15 @@
+import hashlib
+import json
+from time import time
+
+
+# Block class
+class Block(object):
+    def __init__(self):
+        # Block header
+        self.chain = []
+        self.pending_transactions = []
+# Block constructor
+        self.new_block (previous_hash= "Thr Times 01/01/2022 Subxmi brink of second Bailout for Banks.", 
+        proof = 100)
+
